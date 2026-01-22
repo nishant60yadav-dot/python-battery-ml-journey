cfc55d01e62dd561faa86ab2bb1879c9078f300a
Nishant yadav <nishantyadav@Nishants-MacBook-Air.local>
2026-01-22 12:34:10 +0530

    Day 1: Python setup, environment, and first script

---
+++ week1/day1/day1_basics.py
@@ -0,0 +1,16 @@
+# week 1 - day 1
+# Goal: PYTHON Basix in mac os using anaconda
+print("hello world")
+# numbers
+x = 10
+y = 3.5
+print("x =", x)
+print("y =", y)
+#text (string)
+project  = "Battery ML Journey"
+print(project)
+# Simple battery calculation 
+capacity_initial = 2.5 #Ah
+capacity_after = 2.1 #Ah
+capacity_loss = capacity_initial-capacity_after
+print("capacity loss:", capacity_loss, "Ah")
