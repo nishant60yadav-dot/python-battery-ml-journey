0ac9e9b3b40721f39207bd524d9135068a051d08
Nishant yadav <nishantyadav@Nishants-MacBook-Air.local>
2026-02-11 12:57:46 +0530

    Week2 Day 3: Data cleaning, missing value, Duplicates

---
+++ rough.py
@@ -0,0 +1,14 @@
+import pandas as np
+# week 2 - day 1
+# Topic: NumPy basics for numerical data 
+import numpy as np
+capacities_list = [2.50, 2.47, 2.44, 2.40, 2.35]
+capacities_array = np.array(capacities_list)
+print("Python list:", capacities_list)
+print("Numpy array:", capacities_array)
+
+
+
+print("\n Other method")
+capacities_list = np.array([2.50, 2.47, 2.44, 2.40, 2.35])
+print(capacities_list)
